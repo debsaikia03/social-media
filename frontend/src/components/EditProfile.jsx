f22467b3cc61af9290a287bd82d689b3171beab3
debsaikia03 <143481057+debsaikia03@users.noreply.github.com>
2025-06-23 21:43:51 +0530

    ready for deployment

--- frontend/src/components/EditProfile.jsx
+++ frontend/src/components/EditProfile.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react'
 import profilePhoto from '../assets/profile-photo.webp';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/Avatar'
 import { useDispatch, useSelector } from 'react-redux';
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from './ui/select';
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from './ui/Select';
 import { Textarea } from './ui/TextArea'
 import axios from 'axios';
 import Loader from './ui/Loader';
